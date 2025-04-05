8f5150201626df6c3981ddb24f850ff370162692
achahid19 <anasks1999@gmail.com>
2025-04-05 10:49:19 +0000

    update generator

--- generator.cpp
+++ generator.cpp
@@ -8,39 +8,37 @@ using namespace std;
 
 int main() {
 	int	numbersToGenerate;
+	std::set<int> uniqueNumbers;
 
 	std::cout << "How many numbers you want to generate? (approximatively not exact number)";
 	std::cout << std::endl;
 	cin >> numbersToGenerate; cin.ignore();
-    // Seed the random number generator with current time
-    srand(static_cast<unsigned int>(time(0)));
 
-    // Use a set to automatically remove duplicates
-    std::set<int> uniqueNumbers;
+	srand(static_cast<unsigned int>(time(0)));
 
-    for (int i = 0; i < numbersToGenerate + (numbersToGenerate / 2); ++i) {
+	for (int i = 0; i < numbersToGenerate + (numbersToGenerate / 2); ++i) {
 		// Random number between 1 and numbersToGenerate * 2;
-        uniqueNumbers.insert(rand() % (numbersToGenerate * 2) + 1);
-    }
+		uniqueNumbers.insert(rand() % (numbersToGenerate * 2) + 1);
+	}
 
 	std::ofstream	file("random_numbers.txt");
-    if (!file) {
+	if (!file) {
 		std::cerr << "Error: cannot create output file!" << std::endl;
 		return EXIT_FAILURE;
 	}
 
-    auto it = uniqueNumbers.begin();
+	auto it = uniqueNumbers.begin();
 
-    while (it != uniqueNumbers.end()) {
-        file << *it;
-        ++it;
-        if (it != uniqueNumbers.end()) {
-            file << ",";  // Separate numbers by commas
-        }
-    }
+	while (it != uniqueNumbers.end()) {
+		file << *it;
+		++it;
+		if (it != uniqueNumbers.end()) {
+			file << ",";  // Separate numbers by commas
+		}
+	}
 
 	std::cout << "Unique " << uniqueNumbers.size();
 	std::cout << " numbers are generated on random_numbers.txt file" << std::endl;
 
-    return 0;
+	return 0;
 }
